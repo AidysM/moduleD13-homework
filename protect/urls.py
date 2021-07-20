6b6d1456082573b6f73c0c5dd8831b5ee301523d
AidysM <mongush10011979@gmail.com>
2021-07-20 22:06:09 +0700

    created replies.html

--- protect/urls.py
+++ protect/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import ProfileView
+from .views import ProfileView, replies_by_advert#, ReplyList
+# from adverts.views import ReplyListView
+
 
 urlpatterns = [
     path('', ProfileView.as_view()),
+    path('<int:advert_pk>/', replies_by_advert, name='replies_by_advert'),
 ]
--- protect/views.py
+++ protect/views.py
@@ -1,9 +1,19 @@
+import requests
 from django.contrib.auth.models import User
+from django.shortcuts import render
 from django.views.generic import TemplateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 
 from .forms import UserForm
 from adverts.models import Reply, Advert
+
+
+def replies_by_advert(request, advert_pk):
+    # for advert in adverts:
+    adv_replies = Reply.objects.filter(advert=advert_pk).order_by('created_reply')
+    context = {'adv_replies': adv_replies}
+    return render(request, 'protect/replies.html', context)
 
 
 class ProfileView(LoginRequiredMixin, TemplateView):
@@ -22,3 +32,17 @@ class ProfileUpdateView(LoginRequiredMixin, UpdateView):
     form_class = UserForm
     success_url = '/'
 
+
+# class ReplyList(ListView):
+#     model = Reply
+#     template_name = 'protect/replies.html'
+#     # context_object_name = 'replies'
+#     queryset = Reply.objects.order_by('-created_reply')
+#     paginate_by = 10
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         replies = Advert.replies.all()
+#         context['replies'] = replies
+#
+#         return context
